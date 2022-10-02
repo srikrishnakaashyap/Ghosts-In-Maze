653000755f220de9c2e6fcfbee0c66ba2f2c8a2d
Kaashyap <mskrishna.kaashyap@gmail.com>
2022-10-02 19:38:28 -0400

    Agent 2

--- Agent2.py
+++ Agent2.py
@@ -21,28 +21,58 @@ class Agent2:
 
         return pathMap
 
-    def isGhostPresentInPath(self, ghostMap, currPathSet, path, currRow, currCol):
-        distance = len(currPathSet)
+    def isGhostPresentInPath(self, ghostMap, currPathMap, path, currRow, currCol):
+        distance = len(currPathMap)
         isGhostPresent = False
+
         for key, value in ghostMap.items():
 
-            if key in currPathSet:
+            if key in currPathMap:
                 isGhostPresent = True
-                distance = min(
-                    distance, path[key[0]][key[1]][2] - path[currRow][currCol][2]
-                )
+                ghostDistance = path[currRow][currCol][2] - path[key[0]][key[1]][2]
+                if ghostDistance >= -3:
+
+                    distance = min(distance, abs(ghostDistance))
 
         return isGhostPresent, distance
 
-    def replan(self, grid, path, ghostMap, currPathSet, currRow, currCol):
+    def replan(
+        self, grid, path, ghostMap, currPathMap, currRow, currCol, nearestGhostDistance
+    ):
         nextRow = path[currRow][currCol][0]
         nextCol = path[currRow][currCol][1]
         rows = [0, 0, -1, 1]
         cols = [1, -1, 0, 0]
+
+        fRow = nextRow
+        fCol = nextCol
+        distanceToGhost = nearestGhostDistance
         for i in range(4):
-            newrow, newcol = currRow + rows[i], currCol + cols[i]
-            if (newrow, newcol == nextRow, nextCol):
+            newRow, newCol = currRow + rows[i], currCol + cols[i]
+            if (newRow, newCol == nextRow, nextCol):
                 continue
+
+            if (
+                0 <= newRow < len(grid)
+                and 0 <= newCol < len(grid[0])
+                and grid[newRow][newCol] == 0
+            ):
+                pm = self.computePath(newRow, newCol, path)
+                isGhostPresent, distance = self.isGhostPresentInPath(
+                    ghostMap, pm, path, newRow, newCol
+                )
+                if isGhostPresent and distance > distanceToGhost:
+                    distanceToGhost = distance
+                    fRow = newRow
+                    fCol = newCol
+                    currPathMap = pm
+                elif not isGhostPresent:
+                    fRow = newRow
+                    fCol = newCol
+                    currPathMap = pm
+                    break
+
+        return fRow, fCol, currPathMap
 
     def agent2(self, grid, path, ghostMap):
 
@@ -56,9 +86,31 @@ class Agent2:
             if currRow == len(grid) - 1 and currCol == len(grid[0]) - 1:
                 break
 
-            if self.isGhostPresentInPath(ghostMap, currPathMap):
-                # Replan
-                pass
+            isGhostPresent, distance = self.isGhostPresentInPath(
+                ghostMap, currPathMap, path, currRow, currCol
+            )
+
+            if isGhostPresent:
+                currRow, currCol, currPathMap = self.replan(
+                    grid, path, ghostMap, currPathMap, currRow, currCol, distance
+                )
+                self.newGhostMap = defaultdict(int)
+                for key, value in ghostMap.items():
+
+                    g = value
+                    while g > 0:
+                        row = key[0]
+                        col = key[1]
+
+                        newPosition = Utility.moveGhost(row, col, grid)
+                        if (currRow, currCol) == newPosition:
+                            return False, grid, (currRow, currCol), ghostMap
+
+                        self.newGhostMap[newPosition] += 1
+
+                        g -= 1
+
+                ghostMap = copy(self.newGhostMap)
             else:
                 newAgentPosition = path[currRow][currCol]
                 currPathMap[(currRow, currCol)] = True
@@ -74,7 +126,6 @@ class Agent2:
                         col = key[1]
 
                         newPosition = Utility.moveGhost(row, col, grid)
-                        # print(newAgentPosition[:2], newPosition)
                         if newAgentPosition[:2] == newPosition:
                             return False, grid, (currRow, currCol), ghostMap
 
@@ -113,4 +164,4 @@ class Agent2:
 if __name__ == "__main__":
     agent2 = Agent2()
 
-    agent2.findPath(5, 1)
+    agent2.findPath(51, 100)
